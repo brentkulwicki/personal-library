e44127353c33b1cfe3e314cf2ea40d429319ce28
brentkulwicki <37881163+brentkulwicki@users.noreply.github.com>
2019-03-27 07:42:52 -0400

    initial commit for my personal JS library

---
+++ lightbox/js/scripts.js
@@ -0,0 +1,64 @@
+// Open the Modal
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+  }
+  
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("modal-demo");
+    var captionText = document.getElementById("modal-caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
+
+  //event listeners to open the modal when thumbnail is clicked
+  let thumbnails = document.getElementsByClassName('modal-hover-shadow');
+  for (let i = 0; i < thumbnails.length; i++) {
+      let index = i + 1;
+      thumbnails[i].addEventListener('click', function(){
+        openModal();
+        currentSlide(index);
+      });
+  };
+
+  //event listener to close modal
+  let modalClose = document.getElementsByClassName('modal-close');
+  modalClose[0].addEventListener('click', closeModal);
+
+  //event listeners for the next and prev buttons
+  let prevImg = document.getElementsByClassName('prev');
+  prevImg[0].addEventListener('click', function(){
+    plusSlides(-1);
+  });
+  let nextImg = document.getElementsByClassName('next');
+  nextImg[0].addEventListener('click', function(){
+    plusSlides(1);
+  });
